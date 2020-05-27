91f8c4c9ff40a7eefa87e7174673b891e102536c
ram071985 <reidmuchow@gmail.com>
2020-05-27 12:58:24 -0500

    created container, rows and columns in ChatRoom component.

--- API/client/src/ChatRoom.js
+++ API/client/src/ChatRoom.js
@@ -1,1 +1,31 @@
-﻿
+﻿import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from 'react-bootstrap';
+import "./index.css";
+
+
+
+class ChatRoom extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <Container className='main-container' fluid>
+                <Row>
+                    <Col></Col>
+                    <Col xs={10}></Col>
+                </Row>
+            </Container>
+            );
+    }
+
+}
+
+export default ChatRoom;
+
+
