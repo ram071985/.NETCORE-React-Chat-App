0becb0f970cedd41bd335330cbf706fba391525e
ram071985 <reidmuchow@gmail.com>
2020-05-28 13:18:46 -0500

    added colors, text, borders, padding etc in css and changed div structure in html.

--- API/client/src/ChatRoom.js
+++ API/client/src/ChatRoom.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import "./index.css";
 
 
@@ -15,25 +15,62 @@ class ChatRoom extends Component {
 
     render() {
         return (
-           <div class="container">
-  <div class="row justify-content-start">
-    <div class="col-4" id="one">
-      <h1>Users</h1>
-    </div>
-    <div class="col-4" id="two">
-      One of two columns
-    </div>
-  </div>
-  <div class="row justify-content-start">
-   <div class="col-4" id="three">
-
-      One of three columns
-    </div>
-   <div class="col-4" id="four">
-      One of four columns
-    </div>
-                </div>
-                </div>
+      <div class="container">
+        <div class="row">
+          <div class="col" id="one">
+            <h2>Users</h2>
+          </div>
+                    <div class="col" id="two">
+                        <br />
+                        <br />
+                        <h6>Ricky Bobby:</h6>
+                        <p>Can anyone on here please tell me where I can find some shake n' bake??!!!</p>
+                        <br />
+                        <br />
+                        <hr />             
+                        <h6>You:</h6>
+                        <p>Ricky, just got to the grocery store.........</p>
+                        <br />
+                        <br />
+                        <hr />
+                        <h6>Ricky:</h6>
+                        <p>Ten, four!</p>
+                        <br />
+                        <br />
+                        <hr />
+                        <h6>Ricky:</h6>
+                        <p>Can you give me a ride to the grocery store?</p>
+                        <br />
+                        <br />
+                        <hr />
+                        <h6>You:</h6>
+                        <p>Why don't you drive yourself?</p>
+                        <br />
+                        <br />
+                        <hr />
+                        <h6>Ricky:</h6>
+                        <p>I only drive my race car Reid...</p>
+                        <br />
+                        <br />
+                        <hr />
+                        <h6>You:</h6>
+                        <p>That's too bad Ricky, you're a prima donna.</p>
+                        <br />
+                        <br />
+                        <hr />
+          </div> 
+                    <div class="col" id="three">
+                        <br />                       
+                        <h6>You</h6>
+                        <h6>Ricky Bobby</h6>
+          </div>
+          <div class="col" id="four">
+            <Form.Group>
+                            <Form.Control type="text" placeholder="Message" />
+                            </Form.Group>
+          </div>
+        </div>
+      </div>
             );
     }
 
