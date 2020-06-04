5abdda9f9b141dad59cee166fb25c65d1fd87e49
ram071985 <reidmuchow@gmail.com>
2020-06-03 20:23:19 -0500

    resaved commit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 using System.Collections.Generic;
 using API;
+using System.Linq;
 
 [ApiController]
     [Route("api/users")]
@@ -18,25 +19,19 @@ using API;
             user.Add(new Users { id = 3, username = "Rick" });
         }
 
-        [HttpGet]
         public List<Users> Get()
         {
             return user;
         }
 
-        [HttpPost]
         public void Post(Users val)
         {
             user.Add(val);
         }
 
-    [HttpGet]
-    public Users Get()
+    public Users Get(int id)
     {
-        return user.Where(x => x.Id == IDesignTimeMvcBuilderConfiguration).FirstOrDefault();
+        return user.Where(x => x.Id == id).FirstOrDefault();
     }
-
-
-
 }
 
