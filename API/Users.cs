6ab8209ee3edc48da424a876320eaad1d5a52e48
ram071985 <reidmuchow@gmail.com>
2020-06-03 20:24:45 -0500

    legit commit

--- API/Users.cs
+++ API/Users.cs
@@ -4,7 +4,7 @@ namespace API
     public class Users
     {
         public int id { get; set; } = 0;
-
+        public int Id { get; internal set; }
         public string username { get; set; } = "";
     }
 }
