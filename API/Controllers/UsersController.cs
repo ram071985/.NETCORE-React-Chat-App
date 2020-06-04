d4331120309c1b1dbcca855122f55904fd056f63
ram071985 <reidmuchow@gmail.com>
2020-06-03 19:35:38 -0500

    added api endpoints for users.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Collections.Generic;
+using API;
+
+[ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        List<Users> user = new List<Users>();
+
+        public UsersController()
+        {
+            user.Add(new Users { id = 1, username = "Reid" });
+            user.Add(new Users { id = 2, username = "James" });
+            user.Add(new Users { id = 3, username = "Rick" });
+        }
+
+        [HttpGet]
+        public List<Users> Get()
+        {
+            return user;
+        }
+
+        [HttpPost]
+        public void Post(Users val)
+        {
+            user.Add(val);
+        }
+
+    [HttpGet]
+    [Route("api/users/{id}")]
+    public List<Users> Get()
+    {
+        return user;
+    }
+
+
+}
+
