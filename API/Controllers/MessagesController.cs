75d4ed9ef000a302ea5fc260055259eb9aea01b0
ram071985 <reidmuchow@gmail.com>
2020-06-05 17:52:35 -0500

    added npgsql code into controller.

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 using System.Collections.Generic;
+using Npgsql;
 
 namespace API.Controllers
 {
@@ -13,6 +14,7 @@ namespace API.Controllers
     {
         List<Messages> message = new List<Messages>();
 
+
         public MessagesController()
         {
             message.Add(new Messages { id = 1, username = "Reid", text = "Hey there." });
@@ -21,9 +23,19 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public List<Messages> Get()
+        public async System.Threading.Tasks.Task GetAsync()
         {
-            return message;
+            var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
+
+            await using var conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync();
+
+            // Retrieve all rows
+            await using (var cmd = new NpgsqlCommand("SELECT * FROM messages", conn))
+            await using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                    Console.WriteLine(reader.GetString(0));
+
         }
 
         [HttpPost]
