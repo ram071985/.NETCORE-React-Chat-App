18ff8b34f2db0fd7c8672534ea30fcfeb6e1393a
ram071985 <reidmuchow@gmail.com>
2020-06-09 13:54:44 -0500

    added DateTIme.Now to post.

--- API/UserInput.cs
+++ API/UserInput.cs
@@ -3,8 +3,11 @@ namespace API
 {
     public class UserInput
     {
+       
         public int Id { get; set; }
         public int UserId { get; set; }
         public string Text { get; set; }
     }
+
+  
 }
