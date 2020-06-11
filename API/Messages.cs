a6a945ad749c8e6e8c94e34249b984c6f1d24d6f
ram071985 <reidmuchow@gmail.com>
2020-06-11 10:10:27 -0500

    changed data models for new messages table in Postgres.

--- API/Messages.cs
+++ API/Messages.cs
@@ -5,7 +5,7 @@ namespace API
     {
         public int Id { get; set; }
 
-        public string UserName { get; set; }
+        public string UserId { get; set; }
 
         public string Text { get; set; }
 
