657f4b9bd0e41a652f3ba0f4260333084f3fccff
ram071985 <reidmuchow@gmail.com>
2020-06-16 09:31:25 -0500

    working through api/authorization post request.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -13,52 +13,24 @@ using Npgsql;
  {
         List<Users> users = new List<Users>();
 
-    [HttpGet]
-    public async System.Threading.Tasks.Task<List<Users>> GetAsync()
-    {
-        var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
+   
 
-        await using var conn = new NpgsqlConnection(connString);
-        await conn.OpenAsync();
 
-        // Retrieve all rows
-        await using (var cmd = new NpgsqlCommand("SELECT * FROM users", conn))
-        {
-            await using (var reader = await cmd.ExecuteReaderAsync())
+            [HttpPost]
+                public async System.Threading.Tasks.Task PostAsync(UserInputUser userInputUser)
             {
-                List<Users> userList = new List<Users>();
+                var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
 
-                while (await reader.ReadAsync())
+                await using var conn = new NpgsqlConnection(connString);
+                await conn.OpenAsync();
+                using (var cmd = new NpgsqlCommand("INSERT INTO users (user_name, created_date) VALUES (@user_name, @created_date)", conn))
                 {
-                    var user = new Users();
-                    user.Id = (int)reader[0];
-                    user.UserName = reader[1].ToString();       
-                    user.CreatedDate = (DateTime)reader[2];
-                    userList.Add(user);
-
+                    var random = new Random();
+                    cmd.Parameters.AddWithValue("@user_name", userInputUser.UserName);
+                    cmd.Parameters.AddWithValue("@created_date", DateTime.Now);
+                    cmd.ExecuteNonQuery();
                 }
-                return userList;
             }
-        }
-
-    }
-
-
-    [HttpPost]
-        public async System.Threading.Tasks.Task PostAsync(UserInputUser userInputUser)
-    {
-        var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
-
-        await using var conn = new NpgsqlConnection(connString);
-        await conn.OpenAsync();
-        using (var cmd = new NpgsqlCommand("INSERT INTO users (user_name, created_date) VALUES (@user_name, @created_date)", conn))
-        {
-            var random = new Random();
-            cmd.Parameters.AddWithValue("@user_name", userInputUser.UserName);
-            cmd.Parameters.AddWithValue("@created_date", DateTime.Now);
-            cmd.ExecuteNonQuery();
-        }
-    }
 
 
 
