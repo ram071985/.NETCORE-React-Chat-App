e891afc35b2a9322895551266393bab4af113a35
ram071985 <reidmuchow@gmail.com>
2020-06-16 19:11:59 -0500

    added sign up form inside card.

--- API/client/src/LandingPage.js
+++ API/client/src/LandingPage.js
@@ -5,7 +5,10 @@ import {
   Button,
   Card,
   InputGroup,
-  FormControl
+  FormControl,
+  Form,
+  Col,
+  Row
 } from "react-bootstrap";
 import "./index.css";
 import { Circle } from "react-feather";
@@ -19,22 +22,38 @@ class LandingPage extends Component {
   render() {
     return (
       <div>
-        <div className="container launch-container">
-          <Button
-            variant="primary"
-            size="lg"
-            className="mb-4 launch-button"
-          >
-            Launch Chat
-          </Button>{" "}
-          <br />
-        </div>
         <div className="card-container">
-        <Card style={{ width: "18rem" }} className="log-in-card">
+          <Card style={{ width: "18rem" }} className="log-in-card">
             <Card.Body>
-              <Card.Title><h5 className="prompt-title">Welcome To The Gab Chat Room</h5></Card.Title>
-              <Card.Link href="#"><h6 className="prompt-links">Create New Account</h6></Card.Link>
-              <Card.Link href="#"><h6 className="prompt-links">Log In</h6></Card.Link>
+              <Card.Title>
+                <h5 className="prompt-title">
+                  Welcome To <span className="gab-color">The Gab</span> Chat
+                  Room
+                </h5>
+              </Card.Title>
+              <Card.Link href="#">
+                <h6 className="prompt-links">Create New Account</h6>
+              </Card.Link>
+              <Card.Link href="#">
+                <h6 className="prompt-links">Log In</h6>
+              </Card.Link>
+              <Form className="mt-3">
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Choose username</Form.Label>
+                  <Form.Control type="email" placeholder="Enter a new username" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Choose password</Form.Label>
+                  <Form.Control type="password" placeholder="Enter a new password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
             </Card.Body>
           </Card>
         </div>
