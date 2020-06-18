c406179c15b51146e33531ca918b1d6ee40307c6
ram071985 <reidmuchow@gmail.com>
2020-06-17 20:04:38 -0500

    added forms back to LandPage component.

--- API/client/src/LandingPage.js
+++ API/client/src/LandingPage.js
@@ -12,12 +12,41 @@ import {
 } from "react-bootstrap";
 import "./index.css";
 import { Circle } from "react-feather";
+import NewUserForm from "./NewUserForm";
 
 class LandingPage extends Component {
   constructor() {
     super();
     this.state = {};
   }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state.password);
+  };
+
+
+  handleSubmit = () => {
+    axios
+    .get("/api/tweet/topic", {
+      params: { topicname: this.state.entry }
+    })
+    .then(res => {
+      this.setState({
+        query: res.data.statuses
+      });
+    })
+
+  }
+
+  handleClick = () => {
+  
+  };
+
+  handleUserLogIn = () => {};
 
   render() {
     return (
@@ -31,31 +60,45 @@ class LandingPage extends Component {
                   Room
                 </h5>
               </Card.Title>
-              <Card.Link href="#">
+              <Card.Link onClick={this.handleClick}>
                 <h6 className="prompt-links">Create New Account</h6>
               </Card.Link>
               <Card.Link href="#">
                 <h6 className="prompt-links">Log In</h6>
               </Card.Link>
-              <Form className="mt-3">
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Choose username</Form.Label>
-                  <Form.Control type="email" placeholder="Enter a new username" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Choose password</Form.Label>
-                  <Form.Control type="password" placeholder="Enter a new password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
             </Card.Body>
           </Card>
+        </div>
+        <div>
+          <div className="container">
+            <Form className="mt-3">
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Choose username</Form.Label>
+                <Form.Control
+                  type="input"
+                  placeholder="Enter a new username"
+                  name="username"
+                  onChange={this.handleChange}
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Choose password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter a new password"
+                  name="password"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
     );
