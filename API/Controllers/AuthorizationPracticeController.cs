65ab2fddd3eabe5acf7841863d94fb791f86953d
ram071985 <reidmuchow@gmail.com>
2020-06-21 18:17:49 -0500

    working on getting RETURNING column back from sessions table query.

--- API/Controllers/AuthorizationPracticeController.cs
+++ API/Controllers/AuthorizationPracticeController.cs
@@ -14,46 +14,16 @@ namespace API.Controllers
     public class AuthorizationPracticeController : ControllerBase
     {
 
-        [HttpGet]
-        public async System.Threading.Tasks.Task<List<SessionModel>> GetAsync([FromForm] SessionModel sessionModel)
-        {
-            var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
-
-            await using var conn = new NpgsqlConnection(connString);
-            await conn.OpenAsync();
-
- 
-            await using (var cmd = new NpgsqlCommand("SELECT id, session_id, created_date FROM sessions ORDER BY id DESC LIMIT 1;", conn))
-            {
-                await using (var reader = await cmd.ExecuteReaderAsync())
-                {
-                    List<SessionModel> sessionList  = new List<SessionModel>();
-
-                    while (await reader.ReadAsync())
-                    {
-                        var session = new SessionModel();
-                        session.Id = (int)reader[0];
-                        session.SessionId = (int)reader[1];
-                        session.CreatedDate = (DateTime)reader[2];
-                        sessionList.Add(session);
-
-                    }
-                    return sessionList;
-                }
-            }
-
-        }
-
 
         [HttpPost]
-        public async System.Threading.Tasks.Task PostAsync(SessionModel sessionModel)
+        public async System.Threading.Tasks.Task PostAsync([FromForm] SessionModel sessionModel)
         {
             var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
 
        
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
-            using (var cmd = new NpgsqlCommand("INSERT INTO sessions (session_id, created_date) VALUES (@session_id, @created_date)", conn))
+            using (var cmd = new NpgsqlCommand("INSERT INTO sessions (session_id, created_date) VALUES (@session_id, @created_date) RETURNING id", conn))
             {
                 var sessionIdCopy = new SessionModel();
                 var random = new Random();
