2d519f8e9d5d558b1d7517e7711373bead34bd4a
ram071985 <reidmuchow@gmail.com>
2020-06-22 12:26:21 -0500

    working through session_id return solution.

--- API/Controllers/AuthorizationPracticeController.cs
+++ API/Controllers/AuthorizationPracticeController.cs
@@ -16,20 +16,35 @@ namespace API.Controllers
 
 
         [HttpPost]
-        public async System.Threading.Tasks.Task PostAsync([FromForm] SessionModel sessionModel)
+        public async System.Threading.Tasks.Task<List<SessionModel>> PostAsync([FromForm] SessionModel sessionModel)
         {
             var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
-
        
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
-            using (var cmd = new NpgsqlCommand("INSERT INTO sessions (session_id, created_date) VALUES (@session_id, @created_date) RETURNING id", conn))
+
+            using (var cmd = new NpgsqlCommand("INSERT INTO sessions (session_id, created_date) VALUES (@session_id, @created_date) RETURNING session_id", conn))
             {
-                var sessionIdCopy = new SessionModel();
                 var random = new Random();
                 cmd.Parameters.AddWithValue("@session_id", random.Next());
                 cmd.Parameters.AddWithValue("@created_date", DateTime.Now);
                 cmd.ExecuteNonQuery();
+
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    List<SessionModel> sessionList = new List<SessionModel>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        var session = new SessionModel();
+                        session.SessionId = (int)reader[0];
+                        sessionList.Add(session);
+
+                    }
+                    return sessionList;
+                }
+
+      
             }
 
         }
