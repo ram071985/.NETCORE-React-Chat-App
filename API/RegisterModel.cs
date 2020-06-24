7b1dccc3251b81cb35590ee96a40dc3a35ceeedb
ram071985 <reidmuchow@gmail.com>
2020-06-24 10:52:40 -0500

    attempting to send data to server from client and insert into 2 different table rows.

---
+++ API/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace API
+{
+    public class RegisterModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
