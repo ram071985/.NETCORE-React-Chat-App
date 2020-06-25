6f947f8144b000564dcdd68bf6f4c72e93b5d443
ram071985 <reidmuchow@gmail.com>
2020-06-25 11:51:02 -0500

    stuck on converting string explicitly and error handling invalid login form entry.

--- API/Controllers/RegisterController.cs
+++ API/Controllers/RegisterController.cs
@@ -13,27 +13,38 @@ namespace API.Controllers
 
     public class RegisterController : ControllerBase
     {
+        string noText = "Please enter a valid username and password";
+
+
 
 
         [HttpPost]
         public async System.Threading.Tasks.Task<List<SessionModel>> PostAsync([FromForm] SessionModel sessionModel, UserModel userModel)
         {
+            
+
             var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
             var user = new UserModel();
 
+    
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
             using (var userInsertCommand = new NpgsqlCommand("INSERT INTO users (username, password, created_date) VALUES (@username, @password, @created_date) RETURNING id", conn))
             {
+          
                 userInsertCommand.Parameters.AddWithValue("@username", userModel.Username);
                 userInsertCommand.Parameters.AddWithValue("@password", userModel.Password);
                 userInsertCommand.Parameters.AddWithValue("@created_date", DateTime.Now);
 
                 await using (var reader = await userInsertCommand.ExecuteReaderAsync())
                 {
-                    var users = new List<UserModel>();
+                   
                     while (await reader.ReadAsync())
                     {
+                        if (user.Username == "" || user.Password == "")
+                        {
+                            return <string> "fffff";
+                        }
                         user.Id = (int)reader[0];
 
                     }
