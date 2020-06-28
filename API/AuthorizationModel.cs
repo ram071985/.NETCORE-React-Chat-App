dfd78b93cb5422ce0eaac819861e246bb9c6e7b5
ram071985 <reidmuchow@gmail.com>
2020-06-28 18:14:46 -0500

    added AuthorizationController and model.

---
+++ API/AuthorizationModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace API
+{
+    public class AuthorizationModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
--- API/Controllers/AuthorizationController.cs
+++ API/Controllers/AuthorizationController.cs
@@ -3,28 +3,97 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 using System.Collections.Generic;
 using Npgsql;
+using System.Net.Http;
+using Microsoft.Net.Http.Headers;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route("api/authorization")]
+    [Route("api/authorize")]
 
-    public class AuthorizationController : ControllerBase 
+    public class AuthorizationController : ControllerBase
     {
 
 
         [HttpPost]
-        public async System.Threading.Tasks.Task PostAsync(SessionModel session)
+        public async System.Threading.Tasks.Task<List<SessionModel>> PostAsync([FromForm] SessionModel sessionModel, UserModel userModel)
         {
-            var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
 
+            if (userModel.Username == "")
+            {
+                throw new Exception("empty username");
+            }
+            if (userModel.Password == "")
+            {
+                throw new Exception("empty password");
+            }
+
+            var connString = "Host=localhost;Username=reid;Password=Lucy07181985!;Database=chat_app";
+            var user = new UserModel();
+
+    
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
-            using (var cmd = new NpgsqlCommand("INSERT INTO sessions (user_id) VALUES (@user_id)", conn))
+
+            using (var checkUsernameCommand = new NpgsqlCommand("SELECT * FROM users WHERE username = @username", conn))
             {
 
-                cmd.ExecuteNonQuery();
+                checkUsernameCommand.Parameters.AddWithValue("@username", userModel.Username);
+
+
+                await using (var reader = await checkUsernameCommand.ExecuteReaderAsync())
+                {
+
+                    while (await reader.ReadAsync())
+                    {
+                        user.Username = reader[1].ToString();
+                        if(userModel.Username == user.Username)
+                        {
+                            throw new Exception("redundant username");
+                        }
+                   
+                    }
+                }
             }
+
+
+            using (var userInsertCommand = new NpgsqlCommand("INSERT INTO users (username, password, created_date) VALUES (@username, @password, @created_date) RETURNING id", conn))
+            {
+            
+                userInsertCommand.Parameters.AddWithValue("@username", userModel.Username);
+                userInsertCommand.Parameters.AddWithValue("@password", userModel.Password);
+                userInsertCommand.Parameters.AddWithValue("@created_date", DateTime.Now);
+
+                await using (var reader = await userInsertCommand.ExecuteReaderAsync()) 
+                {
+                   
+                    while (await reader.ReadAsync())
+                    {
+                       
+                        user.Id = (int)reader[0];
+                                  
+                    }
+                }
+            }
+
+            using (var sessionInsertCommand = new NpgsqlCommand("INSERT INTO sessions (user_id) VALUES (@userId) RETURNING id", conn))
+            {
+                sessionInsertCommand.Parameters.AddWithValue("@userId", user.Id);
+                await using (var reader = await sessionInsertCommand.ExecuteReaderAsync())
+                {
+                    var sessions = new List<SessionModel>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        var session = new SessionModel();
+                        session.Id = (int)reader[0];
+                        sessions.Add(session);
+
+                    }
+                    return sessions;
+                }
+            }
+
         }
 
     }
