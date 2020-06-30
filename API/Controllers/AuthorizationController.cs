c77c9d8e1de399ef739893ca29f12026d17f0bdc
ram071985 <reidmuchow@gmail.com>
2020-06-29 19:07:03 -0500

    error with session table insertion.

--- API/Controllers/AuthorizationController.cs
+++ API/Controllers/AuthorizationController.cs
@@ -36,60 +36,35 @@ namespace API.Controllers
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
 
-            using (var checkUsernameCommand = new NpgsqlCommand("SELECT username,password FROM users WHERE username = @username AND password = @password", conn))
+            using (var checkUsernameCommand = new NpgsqlCommand("SELECT username FROM users WHERE username = @username", conn))
             {
 
                 checkUsernameCommand.Parameters.AddWithValue("@username", userModel.Username);
-                checkUsernameCommand.Parameters.AddWithValue("@password", userModel.Password);
 
                 await using (var reader = await checkUsernameCommand.ExecuteReaderAsync())
                 {
 
                     while (await reader.ReadAsync())
                     {
-                        user.Username = reader[0].ToString();
-                        user.Password = reader[1].ToString();
-                        if (userModel.Username != user.Username)
+
+                        if (user.Username == user.Username)
                         {
+                            user.Id = (int)reader[0];
+                            user.Username = reader[1].ToString();
+
+                        }
+                        else
+                        {
+
                             throw new Exception("false username");
-
                         }
-                        if (userInputUser.Password != user.Password)
-                        {
-                            throw new Exception("false password");
-
-                        }
-
-                    }
-                }
-            }
-
-
-            using (var checkPasswordCommand = new NpgsqlCommand("SELECT * FROM users WHERE password = @password", conn))
-            {
-            
-         
-                checkPasswordCommand.Parameters.AddWithValue("@password", userModel.Password);
-               
-
-                await using (var reader = await checkPasswordCommand.ExecuteReaderAsync()) 
-                {
-                   
-                    while (await reader.ReadAsync())
-                    {
-                        user.Id = (int)reader[0];
-                        if (userModel.Password != user.Password)
-                        {
-                            throw new Exception("incorrect password");
-                        }
-                                  
                     }
                 }
             }
 
             using (var sessionInsertCommand = new NpgsqlCommand("INSERT INTO sessions (user_id) VALUES (@userId) RETURNING id", conn))
             {
-                sessionInsertCommand.Parameters.AddWithValue("@userId", userModel.Id);
+                sessionInsertCommand.Parameters.AddWithValue("@userId", user.Id);
                 await using (var reader = await sessionInsertCommand.ExecuteReaderAsync())
                 {
                     var sessions = new List<SessionModel>();
@@ -98,6 +73,7 @@ namespace API.Controllers
                     {
                         var session = new SessionModel();
                         session.Id = (int)reader[0];
+
                         sessions.Add(session);
 
                     }
