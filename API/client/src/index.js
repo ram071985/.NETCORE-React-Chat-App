6fbf2ed7c921f630bf6814cf9b45064e33e9f437
ram071985 <reidmuchow@gmail.com>
2020-07-03 06:17:02 -0500

    hooked up redirect in router for chat room.

--- API/client/src/index.js
+++ API/client/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+import ProtectedRoute from "./ProtectedRoute";
+import LandingPage from "./LandingPage";
+import ChatRoom from "./ChatRoom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+    <Switch>
+    <Route path="/login" component={LandingPage} />
+    <ProtectedRoute exact={true} path="/" component={ChatRoom} />
+    <ProtectedRoute component={ChatRoom} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
