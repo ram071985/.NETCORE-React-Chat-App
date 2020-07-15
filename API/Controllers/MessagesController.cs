b23a3373f2d440acb71835fa1b34179c6125fc9b
ram071985 <reidmuchow@gmail.com>
2020-07-15 17:12:45 -0500

    added new CreateMessageService with all the logic from MessagesController.

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -4,6 +4,7 @@ using System.Text.Encodings.Web;
 using System.Collections.Generic;
 using Npgsql;
 using Microsoft.Extensions.Configuration;
+using Core.Services;
 
 namespace API.Controllers
 {
@@ -13,83 +14,24 @@ namespace API.Controllers
 
     public class MessagesController : ControllerBase
     {
-        private string _databaseUserName;
-        private string _databasePassword;
+        private ICreateMessageService _createMessageService;
 
-        public MessagesController(IConfiguration configuration)
+        public MessagesController(IConfiguration configuration, ICreateMessageService createMessageService)
         {
-            _databaseUserName = configuration["Database:Username"];
-            _databasePassword = configuration["Database:Password"];
+            _createMessageService = createMessageService;
         }
 
         [HttpPost]
-        public async System.Threading.Tasks.Task<List<MessageModel>> PostAsync([FromBody] MessageModel messageModel)
+        public MessageModel Post([FromBody] MessageModel messageModel)
         {
 
-            var connString = "Host=localhost;Username=" + _databaseUserName + ";Password=" + _databasePassword + ";Database=chat_app";
+          var message = _createMessageService.GetBackMessage(messageModel.SessionId, messageModel.Text, messageModel.CreatedDate);
 
-            await using var conn = new NpgsqlConnection(connString);
-            await conn.OpenAsync();
-
-            using (var checkUsernameCommand = new NpgsqlCommand("SELECT user_id FROM sessions WHERE id = @id", conn))
+            return new MessageModel
             {
-
-               checkUsernameCommand.Parameters.AddWithValue("@id", messageModel.SessionId);
-
-                await using (var reader = await checkUsernameCommand.ExecuteReaderAsync())
-                {
-
-                    while (await reader.ReadAsync())
-                    {
-
-                        messageModel.SessionId = (int)reader[0];
-
-                    }
-                }
-            }
-
-            
-            using (var messageInsertCommand = new NpgsqlCommand("INSERT INTO messages (user_id, text, created_date) VALUES (@userId, @text, @created_date)", conn))
-            {
-                messageInsertCommand.Parameters.AddWithValue("@userId", messageModel.SessionId);
-                messageInsertCommand.Parameters.AddWithValue("@text", messageModel.Text);
-                messageInsertCommand.Parameters.AddWithValue("@created_date", DateTime.Now);
-
-                await using (var reader = await messageInsertCommand.ExecuteReaderAsync())
-                {
-
-                    while (await reader.ReadAsync())
-                    {
-
-                        messageModel.Text = reader[3].ToString();
-                        messageModel.CreatedDate = (DateTime)reader[4];
-
-                    }
-
-                }
-            } 
-
-            using (var messageInsertCommand = new NpgsqlCommand("SELECT u.username, m.text, m.created_date FROM messages m JOIN users u ON u.id = m.user_id WHERE m.user_id = u.id", conn))
-            {
-
-                await using (var reader = await messageInsertCommand.ExecuteReaderAsync())
-                {
-                    var messages = new List<MessageModel>();
-
-                    while (await reader.ReadAsync())
-                    {
-                        var message = new MessageModel();
-                        message.Username = reader[0].ToString();
-                        message.Text = reader[1].ToString();
-                        message.CreatedDate = (DateTime)reader[2];
-                        messages.Add(message);
-
-
-                    }
-                    return messages;
-                }
-                
-            }
+                Text = message.Text,
+                CreatedDate = message.CreatedDate
+            };
         }
 
         [HttpGet]
@@ -123,7 +65,6 @@ namespace API.Controllers
             }
 
         }
-
 
     }
 }
