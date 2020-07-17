e92d1061a1da59dcdf83ed78859d789b8673ed8d
ram071985 <reidmuchow@gmail.com>
2020-07-17 09:31:45 -0500

    added CreateNewUserSevice.cs

---
+++ Core/Services/CreateNewUserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Core.Services
+{
+    public interface ICreateNewUserService
+    {
+        List<Users> GetUser(int sessionId, string text, DateTime createdDate);
+    }
+    public class CreateNewUserService : ICreateNewUserService
+    {
+        private string _databaseUserName;
+        private string _databasePassword;
+
+        public CreateNewUserService(IConfiguration configuration)
+        {
+            _databaseUserName = configuration["Database:Username"];
+            _databasePassword = configuration["Database:Password"];
+        }
+
+        public List<Users> GetUser(int sessionId, string text, DateTime createdDate)
+        {
+
+        }
+    }
+}
