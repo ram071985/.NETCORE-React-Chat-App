f5da8ec244d32fdb1076fdee2c1938a8cb9dbd42
ram071985 <reidmuchow@gmail.com>
2020-07-26 08:12:17 -0500

    resolved meger conflict with Tests/obj/Debug/netcoreapp3.1/Tests.csprojAssemblyReference.cache.

---
+++ Tests/AuthorizeUserTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Core;
+using NUnit.Framework;
+using static Core.Services.AuthorizeUserService;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AuthorizeUserTest
+    {       
+        private Session _session;
+
+        public void Setup()
+        {
+            _session = new Session();
+        }
+       
+    }
+}
