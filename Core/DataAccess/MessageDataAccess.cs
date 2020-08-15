337675028fde8a2ba47feba4366ef0e424d07b0a
ram071985 <reidmuchow@gmail.com>
2020-08-15 13:19:13 -0500

    continued refactoring.

--- Core/DataAccess/MessageDataAccess.cs
+++ Core/DataAccess/MessageDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Core.Entities;
 using Npgsql;
 
@@ -6,9 +7,35 @@ namespace Core.DataAccess
 {
     public class MessageDataAccess
     {
-        public Message GetMessages(NpgsqlConnection conn, )
+        public List<Message> GetMessages(NpgsqlConnection conn)
         {
+            using (var messageInsertCommand = new NpgsqlCommand("SELECT u.id, u.username, u.password, u.last_active_at, u.created_date, m.text, m.created_date FROM messages m JOIN users u ON u.id = m.user_id WHERE m.user_id = u.id", conn))
+            {
 
+                using (var reader = messageInsertCommand.ExecuteReader())
+                {
+                    var messages = new List<Message>();
+
+                    while (reader.Read())
+                    {
+                        var message = new Message
+                        {
+                            User = new User
+                            {
+                                Id = (int)reader[0],
+                                Username = reader[1].ToString(),
+                                Password = reader[2].ToString(),
+                                LastActiveAt = (DateTime)reader[3],
+                                CreatedDate = (DateTime)reader[4]
+                            },
+                            Text = reader[1].ToString(),
+                            CreatedDate = (DateTime)reader[2],
+                        };
+                        messages.Add(message);
+                    }
+                    return messages;
+                }
+            }
         }
     }
 }
--- Core/Services/CreateMessageService.cs
+++ Core/Services/CreateMessageService.cs
@@ -70,32 +70,7 @@ namespace Core.Services
 
               }
 
-                using (var messageInsertCommand = new NpgsqlCommand("SELECT u.id, u.username, u.password, u.last_active_at, u.created_date, m.text, m.created_date FROM messages m JOIN users u ON u.id = m.user_id WHERE m.user_id = u.id", conn))
-                {
-
-                    using (var reader = messageInsertCommand.ExecuteReader())
-                    {
-                       var messages = new List<Message>();
-
-                        while (reader.Read())
-                        {
-                            var message = new Message {
-                                User = new User
-                                {
-                                    Id = (int)reader[0],
-                                    Username = reader[1].ToString(),
-                                    Password = reader[2].ToString(),
-                                    LastActiveAt = (DateTime)reader[3],
-                                    CreatedDate = (DateTime)reader[4]
-                                },
-                                Text = reader[1].ToString(),
-                                CreatedDate = (DateTime)reader[2],
-                            };                                                                      
-                            messages.Add(message);
-                        }
-                        return messages;
-                    }
-                }
+               
             }
         }
     }
