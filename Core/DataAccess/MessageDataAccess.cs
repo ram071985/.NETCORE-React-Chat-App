50d05d851ad9bdcf617f2395451bf08401c8009a
ram071985 <reidmuchow@gmail.com>
2020-08-14 21:20:16 -0500

    continued refactoring.

---
+++ Core/DataAccess/MessageDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Core.Entities;
+using Npgsql;
+
+namespace Core.DataAccess
+{
+    public class MessageDataAccess
+    {
+        public Message GetMessages(NpgsqlConnection conn, )
+        {
+
+        }
+    }
+}
